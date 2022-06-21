903b733e81e62f7c87f890fc32ddb977e59984ac
rosepearson <rose.pearson@niwa.co.nz>
2022-06-22 02:24:57 +1200

    update the cylc-flow - not using a bash script yet as don't have run permissions, also added hte bash script i'd like to eventually run, and added the updated setup.py - now loads in the LiDAR dataset - doesn't yet download data

---
+++ cylc-src/waikanae/bin/setup.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+#------------------------------------------------------------------------------
+# This script is used to activate a preexisting conda environment
+# ------------------------------------------------------------------------------
+# Note: "conda" does not work with "set -u"
+conda activate --stack cylc-geofabrics
