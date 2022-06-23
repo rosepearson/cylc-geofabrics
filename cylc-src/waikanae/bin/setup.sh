cbf8814dca3c4de0a3b2e0a124a1982db75585d6
rosepearson <rose.pearson@niwa.co.nz>
2022-06-24 00:11:45 +1200

    Updated workflow and rivers python script so running - except for actually calling through to geofabrics. Correctly executs and run to completion :)

--- cylc-src/waikanae/bin/setup.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#------------------------------------------------------------------------------
-# This script is used to activate a preexisting conda environment
-# ------------------------------------------------------------------------------
-# Note: "conda" does not work with "set -u"
-
-set +u # allow unbound variables
-conda activate geofabrics # or whatever
-set -u # disallow them again, before carrying on
