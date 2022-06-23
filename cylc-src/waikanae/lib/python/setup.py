6e5ce48601d6ad5ff4e2d02e3face0ab38921bc2
Rose Pearson <Rose.Pearson@niwa.co.nz>
2022-06-23 21:59:12 +1200

    Updated formatting for the setup.py

--- cylc-src/waikanae/lib/python/setup.py
+++ cylc-src/waikanae/lib/python/setup.py
@@ -9,42 +9,49 @@ import geoapis
 import geoapis.lidar
 import geopandas
 
+
 def main():
-    """ The setup.main function updates the paths in the instruction file based on the run location,
-    and downloads all LiDAR data required for the later GeoFabrics processing steps. """
-    print('Run setup!')
-    
-    # define paths - Note if testing with a direct python run use 'base_path = pathlib.Path().cwd()'
+    """ The setup.main function updates the paths in the instruction file based
+    on the run location,     and downloads all LiDAR data required for the
+    later GeoFabrics processing steps. """
+    print("Run setup!")
+
+    # define paths
+    # Note if calling python direct use: 'base_path = pathlib.Path().cwd()'
     base_path = pathlib.Path().cwd().parent.parent.parent
     data_path = base_path / "data"
-    
+
     # read in the instruction file
     with open(base_path / "instruction.json", "r") as file_pointer:
         instructions = json.load(file_pointer)
     crs = instructions["rivers"]["output"]["crs"]["horizontal"]
-    
-    # amend paths (cache and river files) in instruction file and save back out
+
+    # amend paths (cache and river files) in instruction file
     instructions["rivers"]["data_paths"]["cache_path"] = str(data_path)
-    instructions["rivers"]["rivers"]["rec_file"] = str(data_path / instructions["rivers"]["rivers"]["rec_file"])
-    instructions["rivers"]["rivers"]["flow_file"] = str(data_path / instructions["rivers"]["rivers"]["flow_file"])
+    instructions["rivers"]["rivers"]["rec_file"] = str(
+        data_path / instructions["rivers"]["rivers"]["rec_file"]
+    )
+    instructions["rivers"]["rivers"]["flow_file"] = str(
+        data_path / instructions["rivers"]["rivers"]["flow_file"]
+    )
     instructions["drains"]["data_paths"]["cache_path"] = str(data_path)
     instructions["dem"]["data_paths"]["cache_path"] = str(data_path)
     instructions["roughness"]["data_paths"]["cache_path"] = str(data_path)
-    with open(base_path / 'instruction.json', 'w') as file_pointer: # Override in cylc run cache
+    # Save the amended instructions in cylc run cache
+    with open(base_path / "instruction.json", "w") as file_pointer:
         json.dump(instructions, file_pointer, indent=4)
-    
     # load in catchment
     catchment = geopandas.read_file(data_path / "small.geojson")
-    catchment.set_crs(crs, inplace=True, allow_override=True) # Explicitly override as CRS isn't being read in correctly.
-    
+    # Explicitly override as CRS isn't being read in correctly.
+    catchment.set_crs(crs, inplace=True, allow_override=True)
+
     # load in LiDAR files
-    print('Download LiDAR files')
-    lidar_fetcher = geoapis.lidar.OpenTopography(cache_path=data_path, 
-                                                 search_polygon=catchment, verbose=True)
+    print("Download LiDAR files")
+    lidar_fetcher = geoapis.lidar.OpenTopography(
+        cache_path=data_path, search_polygon=catchment, verbose=True
+    )
     lidar_fetcher.run("Wellington_2013")
-    print('Finished setup!')
-    
-    
+    print("Finished setup!")
 
 
 if __name__ == "__main__":
