2660c7d9d1cdbcdc3a34a3d89f2f62f0e35f6872
rosepearson <rose.pearson@niwa.co.nz>
2023-01-11 16:15:39 +1300

    initial set of changes to the setup to create the instruciton file

--- cylc-src/lib/python/setup.py
+++ cylc-src/lib/python/setup.py
@@ -19,80 +19,87 @@ def main():
 
     print("Run setup!")
 
-    ## Define paths
+    ## Define cylc paths
     # note if calling python direct use: 'base_path = pathlib.Path().cwd()'
     cylc_run_base_path = pathlib.Path().cwd().parent.parent.parent
-    cylc_run_cache_path = cylc_run_base_path / "data"
-    cache_path = pathlib.Path("/nesi/project/niwa03440/cylc_test/")
-    rec_data_path = cache_path / "data/REC2/"
-
-    ## Read in the instruction file
-    with open(cylc_run_base_path / "instruction.json", "r") as file_pointer:
-        instructions = json.load(file_pointer)
-    crs = instructions["rivers"]["output"]["crs"]["horizontal"]
-
-    ## Create results director
-    subfolder = instructions["dem"]["data_paths"]["subfolder"]
-    cylc_run_results_dir = cylc_run_cache_path / subfolder
-    cylc_run_results_dir.mkdir(parents=False, exist_ok=False)
-
-    ## Amend paths (cache and river files) in instruction file
-    # cache_path
-    instructions["rivers"]["data_paths"]["local_cache"] = str(cylc_run_cache_path)
-    instructions["drains"]["data_paths"]["local_cache"] = str(cylc_run_cache_path)
-    instructions["dem"]["data_paths"]["local_cache"] = str(cylc_run_cache_path)
-    instructions["roughness"]["data_paths"]["local_cache"] = str(cylc_run_cache_path)
-    # catchment_boundary path
-    catchment_boundary = instructions["dem"]["data_paths"]["catchment_boundary"]
-    instructions["drains"]["data_paths"]["catchment_boundary"] = str(
-        cylc_run_cache_path / catchment_boundary
-    )
-    instructions["dem"]["data_paths"]["catchment_boundary"] = str(
-        cylc_run_cache_path / catchment_boundary
-    )
-    instructions["roughness"]["data_paths"]["catchment_boundary"] = str(
-        cylc_run_cache_path / catchment_boundary
-    )
-    # final geofabric output to overal cylc cache location
-    output_geofabric_path = cache_path / instructions["roughness"]["data_paths"]["result_geofabric"]
-    instructions["roughness"]["data_paths"]["result_geofabric"] = str(
-        output_geofabric_path
-    )
-    output_geofabric_path.parents[0].mkdir(parents=True, exist_ok=True)
+    cylc_run_cache_path = cylc_run_base_path / "geofabrics_cache"
+    cyclc_run_inputs_path = cylc_run_base_path / "catchments"
+    catchment_id = "029"
     
-    # river flow, friction and network files
-    instructions["rivers"]["rivers"]["rec_file"] = str(
-        rec_data_path / instructions["rivers"]["rivers"]["rec_file"]
-    )
-    instructions["rivers"]["rivers"]["flow_file"] = str(
-        rec_data_path / instructions["rivers"]["rivers"]["flow_file"]
-    )
-
-    ## Load in the LINZ API key and add to the instruction file
+    ## Create results directory
+    subfolder = "results"
+    cylc_run_results_dir = cylc_run_cache_path / subfolder
+    cylc_run_results_dir.mkdir(parents=True, exist_ok=False)
+    
+    ## Define catchment boundary
+    catchment_boundary_path = cyclc_run_inputs_path / "catchments" / catchment_id + "_large.geojson"
+    
+    ## Read in the parameter files
+    with open(cyclc_run_inputs_path / "parameters" / "global.json", "r") as file_pointer:
+        global_parameters = json.load(file_pointer)
+    with open(cyclc_run_inputs_path / "parameters" / catchment_id + ".json", "r") as file_pointer:
+        catchment_parameters = json.load(file_pointer)
+    
+    ## Pull out global paths
+    cache_path = pathlib.Path(global_parameters["shared"]["data_paths"].pop("global_cache"))
+    network_path = cache_path / instructions["rivers"]["rivers"].pop(network_file_relative_path)
+    
+    ## Set global paths
+    global_parameters["shared"]["data_paths"]["local_cache"] = str(cylc_run_cache_path)
+    global_parameters["shared"]["data_paths"]["subfolder"] = subfolder
+    global_parameters["rivers"]["rivers"]["network_file"] = network_path
+    global_parameters["shared"]["data_paths"]["catchment_boundary"] = str(catchment_boundary_path)
+    
+    ## Add in the LINZ API key and add to the instruction file
     # Load the LINZ API keys
     dotenv.load_dotenv(cylc_run_base_path / ".env")
     linz_key = os.environ.get("LINZ_API", None)
     # Add the LINZ API key
-    instructions["rivers"]["apis"]["linz"]["key"] = linz_key
-    instructions["drains"]["apis"]["linz"]["key"] = linz_key
-    instructions["dem"]["apis"]["linz"]["key"] = linz_key
-    instructions["roughness"]["apis"]["linz"]["key"] = linz_key
+    global_parameters["shared"]["apis"]["vector"]["linz"]["key"] = linz_key
 
-    ## Save the amended instructions in cylc run cache
-    with open(cylc_run_base_path / "instruction.json", "w") as file_pointer:
-        json.dump(instructions, file_pointer, indent=4)
-
+    ## Create instruction file from the parameter files
+    # Populate with the global shared values
+    instructions = {"rivers": global_parameters["shared"],
+                    "waterways": global_parameters["shared"],
+                    "dem": global_parameters["shared"],
+                    "roughness": global_parameters["shared"]}
+    # Add the stage specific global values
+    instructions["rivers"] = {**instructions["rivers"],**global_parameters["rivers"]}
+    instructions["waterways"] = {**instructions["waterways"],**global_parameters["waterways"]}
+    instructions["dem"] = {**instructions["dem"],**global_parameters["dem"]}
+    instructions["roughness"] = {**instructions["roughness"],**global_parameters["roughness"]}
+    # Add the stage specific catchment values
+    instructions["rivers"] = {**instructions["rivers"],**catchment_parameters["rivers"]}
+    instructions["waterways"] = {**instructions["waterways"],**catchment_parameters["waterways"]}
+    instructions["dem"] = {**instructions["dem"],**catchment_parameters["dem"]}
+    instructions["roughness"] = {**instructions["roughness"],**catchment_parameters["roughness"]}
+    ## TODO - may need to set above at each level of the dictionary
+    
+    ## Rivers specific setup
+    instructions["rivers"]["data_paths"]["land"] = f"river_catchment_{instructions["rivers"]["rivers"]["area_threshold"]}.geojson"
+    instructions["rivers"]["vector"]["linz"]["land"].pop()
+    instructions["rivers"]["data_paths"]["catchment_boundary"].pop()
+    
+    ## Roughness - set final output to global cache
+    output_geofabric_path = cache_path / catchment_id / "ancil" / "bgflood" / "geofabrics" ## TODO consider pulling out into global - or an unversioned usecase specific json
+    output_geofabric_path.parents[0].mkdir(parents=True, exist_ok=True)
+    instructions["roughness"]["data_paths"]["result_geofabric"] = output_geofabric_path / str(instructions["roughness"]["output"]["grid_params"]["resolution"]) + "_geofabric.nc"
+    
+    
+    ## write out the JSON instruction file
+    with open(cylc_run_base_path / "instruction.json", "w") as json_file:
+        json.dump(instructions, json_file, indent=4)
+    ## TODO - may want to scrub the LINZ key info
+        
     ## Load in catchment
-    catchment = geopandas.read_file(instructions["dem"]["data_paths"]["catchment_boundary"])
-    # Explicitly override as CRS isn't being read in correctly.
-    catchment.set_crs(crs, inplace=True, allow_override=True)
+    crs = global_parameters["shared"]["output"]["crs"]["horizontal"]
+    catchment = geopandas.read_file(catchment_boundary_path)
+    catchment.set_crs(crs, inplace=True, allow_override=True) # TODO - Remove explicite override as CRS isn't being read in correctly.
 
     ## Load in LiDAR files
     print("Download LiDAR files")
-    lidar_fetcher = geoapis.lidar.OpenTopography(
-        cache_path=cylc_run_cache_path, search_polygon=catchment, verbose=True
-    )
-    lidar_fetcher.run("Wellington_2013")
+    lidar_fetcher = geoapis.lidar.OpenTopography(cache_path=cylc_run_cache_path, search_polygon=catchment, verbose=True)
+    lidar_fetcher.run(next(iter(catchment_parameters["apis"]["open_topography"]["lidar"])))
     print("Finished setup!")
 
 
